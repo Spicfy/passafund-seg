8f72707742b55c8d57fa572c703bf4cdf69968ad
Spicfy <68024083+Spicfy@users.noreply.github.com>
2025-02-09 19:50:39 -0500

    finished with posts

--- vue-laravel-main/app/Http/Controllers/PostController.php
+++ vue-laravel-main/app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 {
     //
     public function index(){
-        $posts = Post::with('user')->get();
+        $posts = Post::with('user')->paginate(10);
 
         return response()->json($posts, 200);
     }
